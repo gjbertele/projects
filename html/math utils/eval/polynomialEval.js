613b54531534df8db2bbb5ec81c1f87e55a3dc7e
gjbertele <58128243+gjbertele@users.noreply.github.com>
2024-04-29 19:36:53 -0400

    Update polynomialEval.js

--- html/math utils/eval/polynomialEval.js
+++ html/math utils/eval/polynomialEval.js
@@ -1,8 +1,8 @@
-evaluator.polynomialEval = function(query, parsed, definitions){
+evaluator.polynomialEval = evaluator.normalEval; //function(query, parsed, definitions){
     //factor it
     //list roots
     //minima, maxima
     //derivative
     //plot
     //integral
-}
+//}
